f88a184a65b3d739c821bea69cd7f7246df15895
vandenman <donvdbergh@hotmail.com>
2021-02-10 12:10:34 +0100

    added function for z scores

--- R/friendlyConstructorFunctions.R
+++ R/friendlyConstructorFunctions.R
@@ -9,7 +9,7 @@ replaceNA.numeric <- function(column, replaceWith) {
 
 replaceNA.character <- function(column, replaceWith) {
   charReplaceWith <- as.character(replaceWith)
-  charColumn <- as.character(column) 
+  charColumn <- as.character(column)
   result <- ifelse(is.na(charColumn), charReplaceWith, charColumn)
   return(result)
 }
@@ -18,7 +18,7 @@ replaceNA.factor <- function(column, replaceWith) {
   result <- as.factor(replaceNA.character(column, replaceWith))
   return(result)
 }
-  
+
 replaceNA.ordered <- function(column, replaceWith) {
   result <- reorderFactor(replaceNA.factor(column, replaceWith))
   return(result)
@@ -59,12 +59,12 @@ ifElse.ordered <- function(test, yes, no) {
 
 findDominatingClass <- function(...) {
   args <- list(...)
-  
+
   allTypes <- sapply(args, class)
-  maxType <- max(ordered(allTypes, levels=c("integer", "numeric", "ordered", 
+  maxType <- max(ordered(allTypes, levels=c("integer", "numeric", "ordered",
                                             "factor", "character"))
   )
-  
+
   if (maxType=="numeric" || maxType=="integer") {
     return(as.numeric(1))
   } else if (maxType=="ordered") {
@@ -80,11 +80,11 @@ findDominatingClass <- function(...) {
 
 reorderFactor <- function(someFactor) {
   stopifnot(is.factor(someFactor))
-  
+
   theLevels <- levels(someFactor)
   numericalOrder <- suppressWarnings(as.numeric(theLevels))
   numericalOrderWorks <- all(!is.na(numericalOrder))
-  
+
   if (isTRUE(numericalOrderWorks)) {
     # basically numerics
     orderedLevels <- numericalOrder[ordered(numericalOrder)]
@@ -94,4 +94,20 @@ reorderFactor <- function(someFactor) {
     result <- ordered(someFactor, levels=theLevels[order(theLevels)])
     return(result)
   }
+}
+
+zScores <- function(x) {
+  UseMethod("zScores", x)
+}
+
+zScores.numeric <- function(x) {
+  scale(x)
+}
+
+zScores.factor <- function(x) {
+  allLevelsAreNumeric <- !anyNA(suppressWarnings(numericLevels <- as.numeric(levels(x))))
+  if (allLevelsAreNumeric)
+    return(scale(numericLevels[x]))
+  else # assume this is really what the user wants
+    return(scale(x))
 }
