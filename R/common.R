49b2b54dce4a391bb6202a6bb63be4828ff2c08f
vandenman <donvdbergh@hotmail.com>
2021-02-25 16:15:43 +0100

    removed functions for classical meta-analysis

--- R/common.R
+++ R/common.R
@@ -592,25 +592,6 @@ jaspResultsStrings <- function() {
   }
 }
 
-# USED BY CLASSICALMETAANALYSIS.R
-as.modelTerms <- function(object, ...) UseMethod("as.modelTerms")
-as.modelTerms.list <- function(object) structure(object, class = "modelTerms")
-as.modelTerms.formula <- function(formula) structure(sapply(attr(terms(formula), "term.labels"), strsplit, ":"), class="modelTerms")
-formula.modelTerms <- function(modelTerms, env = parent.frame()) {
-  # Converts a modelTerms list into a one-side R formula
-  #
-  # Args:
-  #   modelTerms:  A list of interaction terms, each term being a list of variable names involved in the interaction
-  #   env:         An environement associated with the variables in the formula, see ?as.formula
-  #
-  # Value:
-  #   A formula. See ?formula
-  #
-  terms = sapply(modelTerms, function(x) paste0(unlist(x), collapse = ":"))
-  terms = terms[terms != ""]
-  formula.rhs = paste(terms, collapse = " + ")
-  if (formula.rhs != "") as.formula(paste(" ~ ", formula.rhs), env = env)
-}
 # PLOT RELATED FUNCTION ----
 .suppressGrDevice <- function(plotFunc) {
   plotFunc <- substitute(plotFunc)
