0e86449c5d683f13bbe8ab6c40ccb42e23f9282b
vandenman <donvdbergh@hotmail.com>
2021-12-07 17:20:23 +0100

    remove whitespace

--- R/commonerrorcheck.R
+++ R/commonerrorcheck.R
@@ -20,7 +20,7 @@
   # Function to gracefully exit an analysis when continuing to run is nonsensical.
   # Comparable to stop(message), except this raises an validationError.
   # Arg message: String with the reason why the analysis has ended.
-  
+
   e <- structure(class = c('validationError', 'error', 'condition'),
                  list(message=message, call=sys.call(-1)))
   stop(e)
@@ -34,13 +34,13 @@
 
   stack <- ''
   if (! is.null(sys.calls()) && length(sys.calls()) >= 9) {
-    
-    stack <- sys.calls()  
+
+    stack <- sys.calls()
     stack <- head(stack[7:length(stack)], -2)
     if (length(stack) > 10) {
       stack <- tail(stack, 10)
     }
-    
+
   }
   e$stackTrace <- stack
   signalCondition(e)
@@ -58,11 +58,11 @@
   #
   # Returns:
   #   String containing the error message.
-  
+
   if (! is.character(type) || length(type) == 0) {
     stop('Non-valid type argument provided')
   }
-  
+
   # Retrieve the error message; spaces indicate that it is already an error message.
   if (grepl(' ', type, fixed=TRUE) == TRUE) {
     message <- type
@@ -70,12 +70,12 @@
     args <- c(list(grouping=grouping, class='error', type=type), list(...))
     message <- do.call(.messages, args)
   }
-  
+
   # Turn the message in a html list item
   if (! is.null(concatenate) || opening == TRUE) {
     message <- paste0('<li>', message, '</li></ul>')
   }
-  
+
   # See if we should concatenate it with something.
   if (is.character(concatenate) && length(concatenate) == 1) {
     endOfString <- substr(concatenate, nchar(concatenate)-4, nchar(concatenate))
@@ -84,7 +84,7 @@
     }
     message <- paste0(concatenate, message)
   }
-  
+
   # See if we should add an opening line.
   if (opening == TRUE) {
     openingMsg <- .messages('error', 'opening')
@@ -111,10 +111,9 @@
       }
     }
   }
-  
+
   return(message)
 }
-
 
 #' @export
 .hasErrors <- function(dataset=NULL, type=NULL, custom=NULL, message='default', exitAnalysisIfErrors=FALSE, ...) {
@@ -129,13 +128,13 @@
   #
   # Returns:
   #   FALSE if no errors were found or a named list specifying for each check which variables violated it as well as a general error message.
-  
+
   if (length(type) == 0 && length(custom) == 0)
     return(FALSE)
-  
+
   if (exitAnalysisIfErrors && message == 'short')
     message <- 'default'
-  
+
   # Error checks definition.
   checks <- list()
   checks[['infinity']] <- list(callback=.checkInfinity, addGroupingMsg=FALSE)
@@ -154,40 +153,40 @@
 
   args <- c(list(dataset=dataset), list(...))
   errors <- list(message=NULL)
-  
+
   # Add info about the custom check functions to the type and checks objects.
   if (length(custom) > 0) {
     if (is.function(custom)) {
       checks[['_custom']] <- list(callback=custom, isCustom=TRUE, hasNamespace=FALSE)
       type <- c(type, '_custom')
     } else if (is.list(custom)) {
-      
+
       if (is.null(names(custom))) {
         names(custom) <- paste0('_custom', seq(length(custom)))
         namespace <- FALSE
       } else {
         namespace <- TRUE
       }
-      
+
       for (i in 1:length(custom)) {
         if (is.function(custom[[i]])) {
           checks[[ names(custom)[i] ]] <- list(callback=custom[[i]], isCustom=TRUE, hasNamespace=namespace)
           type <- c(type, names(custom)[i])
         }
       }
-      
+
     }
   }
-  
+
   for (i in 1:length(type)) {
-    
+
     check <- checks[[ type[[i]] ]]
     if (is.null(check))
       stop('Unknown check type provided: "', type[[i]], '"')
-    
+
     isCustom <- ! is.null(check[['isCustom']]) # Is it an analysis-specific check?
     hasNamespace <- ! isCustom || check[['hasNamespace']] == TRUE # Is it a named check?
-    
+
     # Check the arguments provided/required for this specific check.
     funcArgs <- NULL
     if (hasNamespace) {
@@ -197,7 +196,7 @@
         # Attach the check specific prefix, except for the dataset arg.
         argsExceptDataset <- names(funcArgs) != 'dataset'
         names(funcArgs)[argsExceptDataset] <- paste0(type[[i]], '.', names(funcArgs)[argsExceptDataset])
-        
+
         # Fill in the 'all.*' arguments for this check
         argsAllPrefix <- args[startsWith(names(args), 'all.')]
         if (length(argsAllPrefix) > 0) {
@@ -208,12 +207,12 @@
             }
           }
         }
-        
+
         # See if this check expects target variables and if they were provided, if not add all variables.
         funcTargetVars <- paste0(type[[i]], '.target')
         if (funcTargetVars %in% names(funcArgs) && !funcTargetVars %in% names(args))
           args[[funcTargetVars]] <- .unv(names(dataset))
-        
+
         # Obtain an overview of required and optional check arguments.
         optArgs <- list()
         reqArgs <- list()
@@ -223,21 +222,21 @@
           else
             optArgs <- c(optArgs, funcArgs[a])
         }
-        
+
         if (length(reqArgs) > 0 && all(names(reqArgs) %in% names(args)) == FALSE) {
           missingArgs <- reqArgs[! names(reqArgs) %in% names(args)]
           stop('Missing required argument(s): "', paste(names(missingArgs), collapse=','), '"')
         }
-        
+
         # If .hasErrors() was called when the analysis is not ready, don't perform this error check.
         # Not ready is defined as the dataset being NULL, or being an unnamed data.frame (.readDatasetToEnd() returns this when no variables are specified).
         if (length(reqArgs) > 0 && ("dataset" %in% names(reqArgs) && is.null(dataset) || length(names(dataset)) == 0))
-          next 
-        
+          next
+
         if (length(optArgs) > 0 && all(names(optArgs) %in% names(args)) == FALSE)
           args <- c(args, optArgs[! names(optArgs) %in% names(args)])
       }
-      
+
     }
 
     # Perform the actual error check.
@@ -248,58 +247,58 @@
     } else {
       checkResult <- try(check[['callback']]())
     }
-    
+
     # See if the check itself terminated with an exception (oh dear).
     if (isTryError(checkResult))
       next
-    
+
     # If we don't have an error we can go to the next check.
     if ((! isCustom && checkResult[['error']] != TRUE) || (isCustom && (! is.character(checkResult) || checkResult == '')))
       next
-    
+
     # Create the error message.
     if (! (message == 'short' && ! is.null(errors[['message']]))) {
       opening <- FALSE
       if (is.null(errors[['message']]) && message != 'short')
         opening <- TRUE
-      
+
       varsToAdd <- NULL
       if (! isCustom)
         varsToAdd <- checkResult[['errorVars']]
-      
+
       grouping <- NULL
-      if (! is.null(check[['addGroupingMsg']]) && check[['addGroupingMsg']] == TRUE && 
+      if (! is.null(check[['addGroupingMsg']]) && check[['addGroupingMsg']] == TRUE &&
           ! is.null(args[[ paste0(type[[i]], '.grouping') ]]) )
         grouping <- args[[ paste0(type[[i]], '.grouping') ]]
-      
+
       msgType <- type[[i]]
       if (isCustom)
         msgType <- checkResult
-      
-      errors[['message']] <- base::do.call(.generateErrorMessage, c(list(type=msgType, 
-        opening=opening, concatenate=errors[['message']], variables=varsToAdd, grouping=grouping), 
+
+      errors[['message']] <- base::do.call(.generateErrorMessage, c(list(type=msgType,
+        opening=opening, concatenate=errors[['message']], variables=varsToAdd, grouping=grouping),
         args))
     }
-    
+
     if (! hasNamespace) {
       next  # We won't add info of the error to the list if we don't have a name.
     }
-    
+
     # Add the error (with any offending variables, or TRUE if there were no variables) to the list.
     if (is.list(checkResult) && ! is.null(checkResult[['errorVars']]))
       errors[[ type[[i]] ]] <- checkResult[['errorVars']]
     else
       errors[[ type[[i]] ]] <- TRUE
-    
+
   } # End for-loop.
-  
+
   if (is.null(errors[['message']]))
     return(FALSE)
-  
+
   if (exitAnalysisIfErrors == TRUE)
     .quitAnalysis(errors[['message']])
-  
-  return(errors) 
+
+  return(errors)
 }
 
 
@@ -315,13 +314,13 @@
   # Returns:
   #   Result of the func in vector form when no levels were supplied, otherwise as a single value.
 
-  
+
   if (length(levels) > 0) {
-    
+
     if (length(grouping) != length(levels)) {
       stop('Each grouping variable must have a level specified')
     }
-    
+
     # The levels vector may be a 'mix' of numeric and characters, we need to add additional quotation marks around characters.
     if (is.character(levels)) {
       levels <- vapply(levels, function(x) {
@@ -332,11 +331,11 @@
         }
       }, character(1))
     }
-    
+
     expr <- paste(.v(grouping), levels, sep='==', collapse='&')
     dataset <- subset(dataset, eval(parse(text=expr)))
     result <- func(dataset[[.v(target)]])
-    
+
   } else {
 
     vgrouping <- .v(grouping)
@@ -349,44 +348,44 @@
         }
       },  vtarget)
     result <- result[[ncol(result)]] # The last column holds the func results.
-    
+
   }
-  
+
   return(result)
 }
 
 
 .checkInfinity <- function(dataset, target, grouping=NULL, groupingLevel=NULL) {
-  # Check for infinity in the dataset. 
+  # Check for infinity in the dataset.
   # Args:
   #   dataset: JASP dataset.
   #   target: String vector indicating the target variables.
   #   grouping: String vector indicating the grouping variables.
   #   groupingLevel: Vector indicating the level of each of the grouping variables.
-  
+
   result <- list(error=FALSE, errorVars=NULL)
-  
+
   findInf <- function(x) {
     return(any(is.infinite(x)))
   }
-  
+
   for (v in target) {
-    
+
     if (is.factor(dataset[[.v(v)]])) { # Coerce factor to numeric.
       dataset[[.v(v)]] <- as.numeric(as.character(dataset[[.v(v)]]))
-    } 
-    
-    if (length(grouping) > 0 && length(groupingLevel) > 0) { 
+    }
+
+    if (length(grouping) > 0 && length(groupingLevel) > 0) {
       hasInf <- .applyOnGroups(findInf, dataset, v, grouping, groupingLevel)
     } else { # Makes no sense to check all subgroups for infinity rather than the entire variable at once.
       hasInf <- findInf(dataset[[.v(v)]])
     }
-    
+
     if (hasInf) {
       result$error <- TRUE
       result$errorVars <- c(result$errorVars, v)
     }
-    
+
   }
   return(result)
 }
@@ -398,11 +397,11 @@
   #   dataset: JASP dataset.
   #   target: String vector indicating the target variables.
   #   amount: String vector indicating the amount to check for (e.g. '< 2', or '!= 2').
-  
+
   result <- list(error=FALSE, errorVars=NULL)
 
   for (v in target) {
-    
+
     levelsOfVar <- length(unique(na.omit(dataset[[.v(v)]])))
     for (checkAmount in amount) {
       expr <- paste(levelsOfVar, checkAmount)
@@ -412,23 +411,23 @@
         break
       }
     }
-    
+
   }
   return(result)
 }
 
 
 .checkVariance <- function(dataset, target, equalTo=0, grouping=NULL, groupingLevel=NULL) {
-  # Check for a certain variance in the dataset. 
+  # Check for a certain variance in the dataset.
   # Args:
   #   dataset: JASP dataset.
   #   target: String vector indicating the target variables.
   #   equalTo: Single numeric.
   #   grouping: String vector indicating the grouping variables.
   #   groupingLevel: Vector indicating the level of each of the grouping variables.
-  
+
   result <- list(error=FALSE, errorVars=NULL)
-  
+
   getVariance <- function(x) {
     validValues <- x[is.finite(x)]
     variance <- -1 # Prevents the function from returning NA's
@@ -437,20 +436,20 @@
     }
     return(variance)
   }
-  
+
   for (v in target) {
-    
+
     if (length(grouping) > 0) {
       variance <- .applyOnGroups(getVariance, dataset, v, grouping, groupingLevel)
     } else {
       variance <- getVariance(dataset[[.v(v)]])
     }
-    
+
     if (any(variance == equalTo)) {
       result$error <- TRUE
       result$errorVars <- c(result$errorVars, v)
     }
-    
+
   }
   return(result)
 }
@@ -464,21 +463,21 @@
   #   amount: String vector indicating the amount to check for (e.g. '< 2', or '> 4000').
   #   grouping: String vector indicating the grouping variables.
   #   groupingLevel: Vector indicating the level of each of the grouping variables.
-  
+
   result <- list(error=FALSE, errorVars=NULL)
-  
+
   getObservations <- function(x) {
     return(length(na.omit(x)))
   }
 
   for (v in target) {
-    
+
     if (length(grouping) > 0) {
       obs <- .applyOnGroups(getObservations, dataset, v, grouping, groupingLevel)
     } else {
       obs <- getObservations(dataset[[.v(v)]])
     }
-    
+
     for (checkAmount in amount) {
       expr <- paste(obs, checkAmount)
       if (any(sapply(expr, function(x) eval(parse(text=x))))) { # See if any of the expressions is true.
@@ -487,7 +486,7 @@
         break
       }
     }
-    
+
   }
   return(result)
 }
@@ -503,7 +502,7 @@
   result <- list(error=FALSE, errorVars=NULL)
 
   dataPairs <- dataset[, .v(target)]
-  
+
   if (sum(!apply(dataPairs, 1, function(x){any(is.na(x))})) <= amount) { # See if any of the expressions is true.
     result$error <- TRUE
     result$errorVars <- c(result$errorVars, target)
@@ -517,28 +516,28 @@
 
   # as matrix:
   dataMatrix <- as.matrix(dataset)
-  
+
   # number of rows equal to number of columns?
   if (nrow && nrow(dataset) != ncol(dataset)){
     return(list(error=TRUE,reason="Dataset is not a square matrix"))
   }
-  
+
   # Symmetrical?
   if (symm && !all(round(dataset,10) == t(round(dataset,10)))){
     return(list(error=TRUE,reason="Matrix is not symmetrical"))
   }
-  
+
   # Positive-definite?
   if (posdef && any(round(eigen(dataset)$values,10) <= 0)){
     vars <- .checkForPerfectCorrelations(dataset)
     return(list(error=TRUE,reason="Matrix is not positive-definite", vars=vars))
   }
-  
+
   return(list(error=FALSE, reason = ""))
 }
 
 .checkForPerfectCorrelations <- function(dataset) {
-  
+
   vars <- NULL
   # if not already a correlation matrix
   if (! all(diag(dataset) == 1)) {
@@ -561,24 +560,24 @@
   #   target: String vector indicating the target variables.
   #   min: Number indicating minimum allowed (inclusive)
   #   max: Number indicating maximum allowed (inclusive)
-  
+
   result <- list(error=FALSE, errorVars=NULL)
 
   for (v in target) {
-    
+
     rangeOfVar <- range(na.omit(dataset[[.v(v)]]))
-    
+
     if (rangeOfVar[1] < min || rangeOfVar[2] > max) {
       result$error <- TRUE
       result$errorVars <- c(result$errorvars, v)
     }
-    
+
   }
   return(result)
 }
 
 .checkVarCovData <- function(dataset, target, corFun = NULL, grouping=NULL, corArgs = NULL, ...) {
-  # Check if the matrix returned by corFun is positive definite. 
+  # Check if the matrix returned by corFun is positive definite.
   # internally uses .checkVarCovMatrix
   # Args:
   #   dataset: JASP dataset.
@@ -586,9 +585,9 @@
   #   corFun: a function that calculates a correlation matrix or covariance matrix (e.g. cor or cov are recommended)
   #   corArgs: more arguments to corFun, i.e. use = "pairwise".
   #   grouping: String vector indicating the grouping variables.
-  
+
   result <- list(error=FALSE, errorVars=NULL)
-  
+
   if (is.null(corFun))
     corFun <- stats::cor
 
@@ -600,7 +599,7 @@
     dataset <- split(dataset, groupingData)
   }
   for (d in seq_along(dataset)) {
-    
+
     cormat <- do.call(corFun, c(list(dataset[[d]]), corArgs))
     err <- .checkVarCovMatrix(cormat, nrow = FALSE, symm = FALSE)
     result$error <- err$error
@@ -609,7 +608,7 @@
     if (result$error) # stop at first error
       break
   }
-  
+
   return(result)
 }
 
@@ -627,13 +626,13 @@
 
     if (is.null(names(term)) || !"components" %in% names(term))
       stop("the modelterms list must contain the named item `components`")
-      
+
     components <- term$components
     if (length(components) < 2)
       next
 
     numChildren <- 2^length(components) - 1
-    inclusion <- sapply(modelTerms, 
+    inclusion <- sapply(modelTerms,
       function(termToFind) {
           componentsToFind <- termToFind$components
           return(sum(componentsToFind %in% components) == length(componentsToFind))
@@ -649,70 +648,70 @@
 }
 
 .checkNegativeValues <- function(dataset, target, grouping=NULL, groupingLevel=NULL) {
-  # Check for negative values in the dataset. 
+  # Check for negative values in the dataset.
   # Args:
   #   dataset: JASP dataset.
   #   target: String vector indicating the target variables.
   #   grouping: String vector indicating the grouping variables.
   #   groupingLevel: Vector indicating the level of each of the grouping variables.
-  
+
   result <- list(error=FALSE, errorVars=NULL)
-  
+
   findNegativeValues <- function(x) {
     return(any(na.omit(x) < 0))
   }
-  
+
   for (v in target) {
-    
+
     if (is.factor(dataset[[.v(v)]])) { # Coerce factor to numeric.
       dataset[[.v(v)]] <- as.numeric(as.character(dataset[[.v(v)]]))
-    } 
-    
-    if (length(grouping) > 0 && length(groupingLevel) > 0) { 
+    }
+
+    if (length(grouping) > 0 && length(groupingLevel) > 0) {
       hasNegativeValues <- .applyOnGroups(findNegativeValues, dataset, v, grouping, groupingLevel)
     } else {
       hasNegativeValues <- findNegativeValues(dataset[[.v(v)]])
     }
-    
+
     if (hasNegativeValues) {
       result$error <- TRUE
       result$errorVars <- c(result$errorVars, v)
     }
-    
+
   }
-  
+
   return(result)
 }
 
 .checkMissingValues <- function(dataset, target, grouping=NULL, groupingLevel=NULL) {
-  # Check for missing values in the dataset. 
+  # Check for missing values in the dataset.
   # Args:
   #   dataset: JASP dataset.
   #   target: String vector indicating the target variables.
   #   grouping: String vector indicating the grouping variables.
   #   groupingLevel: Vector indicating the level of each of the grouping variables.
-  
+
   result <- list(error=FALSE, errorVars=NULL)
-  
+
   findMissingValues <- function(x) {
     return(anyNA(x))
   }
-  
+
   for (v in target) {
 
-    if (length(grouping) > 0 && length(groupingLevel) > 0) { 
+    if (length(grouping) > 0 && length(groupingLevel) > 0) {
       hasMissingValues <- .applyOnGroups(findMissingValues, dataset, v, grouping, groupingLevel)
     } else {
       hasMissingValues <- findMissingValues(dataset[[.v(v)]])
     }
-    
+
     if (hasMissingValues) {
       result$error <- TRUE
       result$errorVars <- c(result$errorVars, v)
     }
-    
+
   }
-  
+
   return(result)
 }
 
