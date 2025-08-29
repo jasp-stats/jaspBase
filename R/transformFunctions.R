dbd1afbf06d8cbc7d62b80eb7dd7390a9001bcc8
Kucharssim <kucharssim@gmail.com>
2025-08-29 16:48:16 +0200

    add yeojohnsonauto

--- R/transformFunctions.R
+++ R/transformFunctions.R
@@ -21,31 +21,6 @@ powerTransform <- function(x, lambda, shift = 0) {
 }
 
 #' @export
-YeoJohnson <- function(x, lambda) {
-  result <- mapply(function(xx, ll) {
-    if(xx >= 0) {
-      xp1 <- xx + 1
-      if(ll == 0) {
-        return(log(xp1))
-      } else {
-        return((xp1^ll - 1)/ll)
-      }
-    } else {
-      mxp1 <- -xx+1
-      if(ll == 2) {
-        return(-log(mxp1))
-      } else {
-        tml  <- 2-ll
-        num <- (mxp1^tml) - 1
-        return(-num/tml)
-      }
-    }
-  }, xx = x, ll = lambda)
-
-  return(result)
-}
-
-#' @export
 logit <- function(x) { stats::qlogis(x) }
 
 #' @export
